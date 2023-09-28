d72984c059070acc55f4fd44247323ef3ec1c04c
Matias Almeida <matiasleonelalmeida97@gmail.com>
2023-09-28 01:34:47 -0300

    implementing search event with ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
       type="text"
       class="form-control"
       placeholder="Buscar gifs..."
-      (keyup.enter)="searchTag(txtTagInput.value)"
+      (keyup.enter)="searchTag()"
       #txtTagInput>
   `
 })
 
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
   constructor() { }
 
-  searchTag( newTag: string): void {
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
     console.log({ newTag });
   }
 }
